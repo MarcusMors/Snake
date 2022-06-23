721bb29c416a8b93dc3615585bd69e662deed374
Jose Vilca Campana @marcusmors <alivezeh@gmail.com>
2022-06-23 00:26:00 -0500

    :tada: it begins and probably ends here.

---
+++ src/example_dropdown.cpp
@@ -0,0 +1,59 @@
+// Copyright (C) 2022 José Enrique Vilca Campana
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#include <string>  // for basic_string, string, allocator
+#include <vector>  // for vector
+
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Dropdown, Horizontal, Vertical
+#include "ftxui/component/screen_interactive.hpp"  // for ScreenInteractive
+
+int main(int argc, const char* argv[]) {
+  using namespace ftxui;
+
+  std::vector<std::string> entries = {
+      "tribute",     "clearance", "ally",        "bend",        "electronics",
+      "module",      "era",       "cultural",    "sniff",       "nationalism",
+      "negotiation", "deliver",   "figure",      "east",        "tribute",
+      "clearance",   "ally",      "bend",        "electronics", "module",
+      "era",         "cultural",  "sniff",       "nationalism", "negotiation",
+      "deliver",     "figure",    "east",        "tribute",     "clearance",
+      "ally",        "bend",      "electronics", "module",      "era",
+      "cultural",    "sniff",     "nationalism", "negotiation", "deliver",
+      "figure",      "east",
+  };
+
+  int selected_1 = 0;
+  int selected_2 = 0;
+  int selected_3 = 0;
+  int selected_4 = 0;
+
+  auto layout = Container::Vertical({
+      Container::Horizontal({
+          Dropdown(&entries, &selected_1),
+          Dropdown(&entries, &selected_2),
+      }),
+      Container::Horizontal({
+          Dropdown(&entries, &selected_3),
+          Dropdown(&entries, &selected_4),
+      }),
+  });
+
+  auto screen = ScreenInteractive::FitComponent();
+  screen.Loop(layout);
+}
+
+// Copyright 2020 Arthur Sonzogni. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <limits>
+#include <random>
 
 #include "ftxui/dom/elements.hpp"
 #include "ftxui/screen/screen.hpp"
 #include "ftxui/screen/string.hpp"
 
 int main(void) {
+  using Seed = std::random_device;
+  using Engine = std::default_random_engine;
+  // long, unsigned short, unsigned int, unsigned long, or unsigned long long
+  using int_type = int;
+  using Distribution = std::uniform_int_distribution<int_type>;
+
+  Seed seed;
+  Engine engine{seed()};
+  // const int_type max = std::numeric_limits<int_type>::max();
+  // const int_type min = std::numeric_limits<int_type>::min();
+  const int_type max = 4;
+  const int_type min = 0;
+
+  Distribution distribution(min, max);
+  enum direction : int_type {
+    left = 0,
+    up = 1,
+    right = 2,
+    down = 3,
+  };
+  auto generate_random_number = [&]() { return distribution(engine); };
+
   using namespace ftxui;
 
   auto summary = [&] {
